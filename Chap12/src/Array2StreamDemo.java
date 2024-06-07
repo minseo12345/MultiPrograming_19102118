809fed6b055245bc8d36de70d98dc524c9f2a3d8
박민서 <qkrals2475@naver.com>
2024-06-07 23:08:38 +0900

    12장 연습문제 및 예제

---
+++ Chap12/src/Array2StreamDemo.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
+public class Array2StreamDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] ia = {2,3,5,7,11,13};
+		IntStream is = Arrays.stream(ia);
+		
+		String [] strings 
+		= {"The","pen","is","mighter","than","the","sword"};
+		
+		double [] da = {1.2,3.14,5.8,0.2};
+		DoubleStream ds = DoubleStream.of(da);
+	}
+
+}
